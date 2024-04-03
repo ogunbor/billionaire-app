ab0caecc5dae3dbef8b4e3406b37ba29c86c7e13
mompei <ogunborjerry@gmail.com>
2024-04-03 14:19:04 +0100

    Converted to statefulwidget and add setstate

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key); // Removed const
 
-  void aFunction() {
-    print("Button is pressed");
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  double balance = 0;
+
+  void addMoney() {
+    setState(() {
+      balance += 500;
+    });
   }
 
   @override
@@ -16,40 +25,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.dark(useMaterial3: true),
       home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text("Billionaire App"),
-          ),
-          body: Container(
-              padding: const EdgeInsets.all(20),
-              color: Colors.blueGrey[700],
-              width: double.infinity,
-              height: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Expanded(
-                    flex: 9,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Bank Balance: "),
-                        SizedBox(height: 20),
-                        Text("0"),
-                      ],
-                    ),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Billionaire App"),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          color: Colors.blueGrey[700],
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 9,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Bank Balance: "),
+                    SizedBox(height: 20),
+                    Text('$balance'),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red[700],
+                    minimumSize: const Size(double.infinity, 0),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.red[700],
-                            minimumSize: const Size(double.infinity, 0)),
-                        onPressed: aFunction,
-                        child: const Text("Add Money")),
-                  )
-                ],
-              ))),
+                  onPressed: addMoney,
+                  child: const Text("Add Money"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
